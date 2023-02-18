de56c4d9b3b6b77127ecabcdea2b20874d6ea329
Travis Littlechilds <travislittlechilds@gmail.com>
2023-02-18 15:00:15 +0000

    intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# FX Rate updater
+
+personal project for updating the FX rates used on a daily basis for future reference. Saved to mariaDB running on my home server.
+
+Rates from the exchangerate-api.com free tier plan
---
+++ fxrates/main.py
@@ -0,0 +1,89 @@
+import os
+import requests
+
+from peewee import *
+from datetime import date, datetime
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dbName = os.getenv("DBNAME", "database")
+hostname = os.getenv("HOST", "localhost")
+port = int(os.getenv("PORT", 3336))
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+apikey = os.getenv("APIKEY")
+
+fxUrl = f"https://v6.exchangerate-api.com/v6/{apikey}/latest/EUR"
+
+fxResponse = requests.get(fxUrl)
+fxData = fxResponse.json()
+
+update_date = datetime.utcfromtimestamp(fxData["time_last_update_unix"]).date()
+
+try:
+    database = MySQLDatabase(
+        dbName,
+        thread_safe=True,
+        autorollback=True,
+        field_types=None,
+        operations=None,
+        autocommit=False,
+        autoconnect=True,
+        **{
+            "charset": "utf8",
+            "sql_mode": "PIPES_AS_CONCAT",
+            "use_unicode": True,
+            "host": hostname,
+            "port": port,
+            "user": user,
+            "password": password,
+        },
+    )
+except:
+    pass
+
+
+class UnknownField(object):
+    def __init__(self, *_, **__):
+        pass
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Rate(BaseModel):
+    date_ = DateField()
+    currency = FixedCharField(max_length=3)
+    to_eur = FloatField()
+
+    class Meta:
+        table_name = "rates"
+        indexes = ((("date_", "currency"), True),)
+
+
+if database.is_closed():
+    database.connect()
+
+updateSuccess = 0
+updateError = 1
+
+for i in fxData["conversion_rates"]:
+    try:
+        with database.atomic():
+            new_rate = Rate(
+                date_=update_date, currency=i, to_eur=fxData["conversion_rates"][i]
+            )
+            new_rate.save()
+            updateSuccess += 1
+    except IntegrityError:
+        updateError += 1
+        continue
+
+database.close()
+print(
+    f"fxRates updated {date.today()} with {updateSuccess} successes and {updateError} errors"
+)
